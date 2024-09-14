9f21d260faad77dacb1cabe2482f4518213d8d53
Hamza Jedidi <hamza.jedidi@logient.com>
2024-09-14 01:02:10 +0100

    login page tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 // src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TabViewModule } from 'primeng/tabview';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { PasswordModule } from 'primeng/password';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -20,6 +36,7 @@ export function tokenGetter() {
   declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     JwtModule.forRoot({
@@ -29,8 +46,23 @@ export function tokenGetter() {
         disallowedRoutes: ['http://localhost:3000/api/auth/login'],
       },
     }),
+    DividerModule,
+    ButtonModule,
+    InputTextModule,
+    TabViewModule,
+    InputIconModule,
+    IconFieldModule,
+    PasswordModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    FloatLabelModule
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  providers: [provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +12,13 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  newEmail: string = '';
+  newPassword: string = '';
+  confirmNewPassword: string = '';
+  fullName: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
-
+  constructor(private authService: AuthService, private router: Router) { }
+  activeIndex: number = 1;
   login() {
     this.authService.login(this.email, this.password).subscribe({
       next: (response) => {
@@ -24,5 +29,44 @@ export class LoginComponent {
         console.error('Login failed', error);
       },
     });
+  }
+  signUp() { }
+
+  goToSignIn() {
+    // Navigate to the sign-in page or perform any other action
+    this.activeIndex = 0
+  }
+  goToLogin() {
+    this.activeIndex = 1;
+  }
+
+  onTabChange(event: any) {
+    const tabContent = document.querySelector('.p-tabview-panels');
+    if (tabContent) {
+      tabContent.classList.add('tab-leave');
+      setTimeout(() => {
+        this.activeIndex = event.index;
+        tabContent.classList.remove('tab-leave');
+        tabContent.classList.add('tab-enter');
+        setTimeout(() => {
+          tabContent.classList.remove('tab-enter');
+        }, 500);
+      }, 500);
+    }
+  }
+  tabs = ['First', 'Second', 'Third'];
+  selected = new FormControl(0);
+
+  addTab(selectAfterAdding: boolean) {
+    this.tabs.push('New');
+
+    if (selectAfterAdding) {
+      this.selected.setValue(this.tabs.length - 1);
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+    this.selected.setValue(index);
   }
 }
